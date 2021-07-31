602daf335304d7e294e5cdaaa02e29ffcf2d98a4
Ítalo Franco <italo.ravina@gmail.com>
2021-07-31 12:21:17 +0200

    styles: add global style using styled-components

--- src/app/core/index.jsx
+++ src/app/core/index.jsx
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './index.css';
+import GlobalStyle from '../../styles/global';
 import './App.css';
 
 const Core = () => {
   return (
-    <div className="App" id="core">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <div className="App" id="core">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </>
   );
 };
 
